7faf5c62ba261ded4e5df409fbd590e687ac9871
AndrewQ <amura.cxg@gmail.com>
2023-06-22 04:38:57 +0000

    - Added support for opening entities - Added delay for when an opening changes - General code cleanup

--- custom_components/yas_thermostat/climate.py
+++ custom_components/yas_thermostat/climate.py
@@ -28,6 +28,7 @@ from homeassistant.const import (
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
     STATE_ON,
+    STATE_OPEN,
     EVENT_HOMEASSISTANT_START,
     UnitOfTemperature,
 )
@@ -39,6 +40,7 @@ from homeassistant.components.climate.const import (
     ATTR_TARGET_TEMP_LOW,
     ATTR_FAN_MODE,
     ATTR_HVAC_MODE,
+    ATTR_PRESET_MODE,
     ATTR_PRESET_MODES,
     ClimateEntityFeature,
 )
@@ -51,20 +53,26 @@ from .const import (
     ATTR_DEFAULT_PRESET,
     ATTR_TEMP_TOLERANCE,
     ATTR_TEMP_STEP,
-    ATTR_MIN_CYCLE_DURATION,
+    ATTR_OPENING_ENTITIES,
+    ATTR_OPENING_DELAY,
+    ATTR_CYCLE_DELAY,
+    ATTR_DEFAULT_HVAC_MODE,
+    ATTR_DEFAULT_FAN_MODE,
     ATTR_MANUAL_FAN_MODE,
     ATTR_MANUAL_HVAC_MODE,
     ATTR_MANUAL_TEMP_LOW,
     ATTR_MANUAL_TEMP_HIGH,
-    ATTR_LAST_CYCLE,
     FanMode,
 )
 
 _LOGGER = logging.getLogger(__name__)
 DEFAULT_TEMP_MIN = 7
 DEFAULT_TEMP_MAX = 35
-DEFAULT_MIN_CYCLE_DURATION = timedelta(minutes=5)
+DEFAULT_CYCLE_DELAY = timedelta(minutes=5)
+DEFAULT_OPENING_DELAY = timedelta(seconds=30)
 DEFAULT_TEMP_TOLERANCE = 0.75
+DEFAULT_FAN_MODE = FanMode.OFF
+DEFAULT_HVAC_MODE = HVACMode.OFF
 
 PRESET_SCHEMA = vol.Schema(
     {
@@ -88,18 +96,33 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(ATTR_NAME): cv.string,
         vol.Required(ATTR_TEMP_SENSOR): cv.entity_id,
+        vol.Required(ATTR_PRESET_MODES): vol.All(cv.ensure_list, [PRESET_SCHEMA]),
+        # Optional Values
         vol.Optional(ATTR_COOLER_SWITCH): cv.entity_id,
         vol.Optional(ATTR_HEATER_SWITCH): cv.entity_id,
         vol.Optional(ATTR_FAN_SWITCH): cv.entity_id,
-        vol.Optional(ATTR_DEFAULT_PRESET): cv.string,
+        vol.Optional(ATTR_OPENING_ENTITIES): cv.entity_ids,
         vol.Optional(ATTR_MIN_TEMP): vol.Coerce(float),
         vol.Optional(ATTR_MAX_TEMP): vol.Coerce(float),
         vol.Optional(ATTR_TEMP_STEP): vol.Coerce(float),
-        vol.Optional(ATTR_MIN_CYCLE_DURATION): vol.All(
+        vol.Optional(ATTR_OPENING_DELAY): vol.All(
             cv.time_period, cv.positive_timedelta
         ),
+        vol.Optional(ATTR_CYCLE_DELAY): vol.All(cv.time_period, cv.positive_timedelta),
         vol.Optional(ATTR_TEMP_TOLERANCE): vol.Coerce(float),
-        vol.Required(ATTR_PRESET_MODES): vol.All(cv.ensure_list, [PRESET_SCHEMA]),
+        vol.Optional(ATTR_DEFAULT_PRESET): cv.string,
+        vol.Optional(ATTR_DEFAULT_HVAC_MODE): vol.In(
+            [
+                HVACMode.COOL,
+                HVACMode.HEAT,
+                HVACMode.OFF,
+                HVACMode.HEAT_COOL,
+                HVACMode.FAN_ONLY,
+            ]
+        ),
+        vol.Optional(ATTR_DEFAULT_FAN_MODE): vol.In(
+            [FanMode.ON, FanMode.OFF, FanMode.AUTO]
+        ),
     }
 )
 
@@ -118,6 +141,8 @@ async def async_setup_platform(
 ) -> None:
     """Setup YAS Thermostat Platform"""
     name: str = config[ATTR_NAME]
+    default_hvac_mode = config.get(ATTR_DEFAULT_HVAC_MODE, DEFAULT_HVAC_MODE)
+    default_fan_mode = config.get(ATTR_DEFAULT_FAN_MODE, DEFAULT_FAN_MODE)
     presets = dict(
         map(
             lambda p: (
@@ -125,8 +150,8 @@ async def async_setup_platform(
                 ClimateSettings(
                     p.get(ATTR_TARGET_TEMP_LOW),
                     p.get(ATTR_TARGET_TEMP_HIGH),
-                    p.get(ATTR_HVAC_MODE, HVACMode.HEAT_COOL),
-                    p.get(ATTR_FAN_MODE, FanMode.AUTO),
+                    p.get(ATTR_HVAC_MODE, default_hvac_mode),
+                    p.get(ATTR_FAN_MODE, default_fan_mode),
                 ),
             ),
             config[ATTR_PRESET_MODES],
@@ -135,6 +160,7 @@ async def async_setup_platform(
     heater_switch_id = config.get(ATTR_HEATER_SWITCH)
     cooler_switch_id = config.get(ATTR_COOLER_SWITCH)
     fan_switch_id = config.get(ATTR_FAN_SWITCH)
+    opening_entity_ids = config.get(ATTR_OPENING_ENTITIES)
     default_preset: str = config.get(ATTR_DEFAULT_PRESET, next(iter(presets)))
     temp_sensor_id = config.get(ATTR_TEMP_SENSOR)
     temp_unit: UnitOfTemperature = hass.config.units.temperature_unit
@@ -142,9 +168,8 @@ async def async_setup_platform(
     temp_max: float = config.get(ATTR_MAX_TEMP, DEFAULT_TEMP_MAX)
     temp_tolerance: float = config.get(ATTR_TEMP_TOLERANCE, DEFAULT_TEMP_TOLERANCE)
     temp_step: float = config.get(ATTR_TEMP_STEP, 1.0)
-    min_cycle_duration: timedelta = config.get(
-        ATTR_MIN_CYCLE_DURATION, DEFAULT_MIN_CYCLE_DURATION
-    )
+    cycle_delay: timedelta = config.get(ATTR_CYCLE_DELAY, DEFAULT_CYCLE_DELAY)
+    opening_delay: timedelta = config.get(ATTR_OPENING_DELAY, DEFAULT_OPENING_DELAY)
 
     entities = [
         YetAnotherSmartThermostat(
@@ -153,14 +178,18 @@ async def async_setup_platform(
             heater_switch_id,
             cooler_switch_id,
             fan_switch_id,
+            opening_entity_ids,
             temp_min,
             temp_max,
             temp_unit,
             temp_tolerance,
             temp_step,
-            min_cycle_duration,
+            cycle_delay,
+            opening_delay,
             presets,
             default_preset,
+            default_fan_mode,
+            default_hvac_mode,
         )
     ]
     async_add_entities(entities, update_before_add=True)
@@ -169,24 +198,33 @@ async def async_setup_platform(
 class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
     """Thermostat Class"""
 
-    _presets: dict[str, ClimateSettings]
+    # Settings
     _heater_switch_id: str | None = None
     _cooler_switch_id: str | None = None
-    _fan_switch_id: str | None = None
-    _current_settings: ClimateSettings
-    _current_preset: str | None = None
-    _current_temp: float | None = 12.34
     _temp_sensor_id: str
+    _fan_switch_id: str | None = None
+    _opening_entity_ids: list[str] | None = None
+    _presets: dict[str, ClimateSettings]
     _temp_min: float
     _temp_max: float
     _temp_unit: UnitOfTemperature
     _temp_tolerance: float
     _temp_step: float
-    _min_cycle_duration: timedelta
-    _last_cycle: datetime | None = None
+    _cycle_delay: timedelta
+    _opening_delay: timedelta = timedelta(seconds=30)
+
+    # Current values
+    _current_settings: ClimateSettings
+    _current_preset: str | None = None
+    _current_temp: float | None = None
+    _current_opening_states: dict[str, bool] = {}
     _is_heater_active: bool = False
     _is_cooler_active: bool = False
     _is_fan_active: bool = False
+    _is_initialized: bool = False
+    _openings_locked_value: bool | None = None
+    _openings_lock_expiry: datetime | None = None
+    _cycle_lock_expiry: datetime | None = None
 
     _supported_features: ClimateEntityFeature = (
         ClimateEntityFeature.PRESET_MODE | ClimateEntityFeature.TARGET_TEMPERATURE_RANGE
@@ -209,28 +247,36 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
         heater_entity_id: str | None,
         cooler_entity_id: str | None,
         fan_entity_id: str | None,
+        opening_entity_ids: list[str] | None,
         temp_min: float | None,
         temp_max: float | None,
         temp_unit: UnitOfTemperature,
         temp_tolerance: float,
         temp_step: float,
-        min_cycle_duration: timedelta,
+        cycle_delay: timedelta,
+        opening_delay: timedelta,
         presets: dict[str, ClimateSettings],
-        initial_preset: str,
+        default_preset: str,
+        default_hvac_mode: HVACMode,
+        default_fan_mode: FanMode,
     ) -> None:
         """Initializes a new instance of the YetAnotherSmartThermostat class"""
         self._name = name
         self._presets = presets
+        self._temp_sensor_id = temp_sensor_id
         self._heater_switch_id = heater_entity_id
         self._cooler_switch_id = cooler_entity_id
         self._fan_switch_id = fan_entity_id
-        self._temp_sensor_id = temp_sensor_id
+        self._opening_entity_ids = opening_entity_ids
         self._temp_min = temp_min
         self._temp_max = temp_max
         self._temp_unit = temp_unit
         self._temp_tolerance = temp_tolerance
         self._temp_step = temp_step
-        self._min_cycle_duration = min_cycle_duration
+        self._cycle_delay = cycle_delay
+        self._opening_delay = opening_delay
+        self._default_hvac_mode = default_hvac_mode
+        self._default_fan_mode = default_fan_mode
 
         # Setup modes and features
         if fan_entity_id is not None:
@@ -245,8 +291,8 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
             self._available_hvac_modes.append(HVACMode.HEAT_COOL)
 
         # Initialize the default preset
-        self._current_preset = initial_preset
-        self._current_settings = self._presets[initial_preset].clone()
+        self._current_preset = default_preset
+        self._current_settings = self._presets[default_preset].clone()
 
     async def async_added_to_hass(self) -> None:
         """Run when entity about to be added."""
@@ -273,6 +319,37 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
                 )
             )
 
+        # Setup the listener for the openings if they are set
+        if self._opening_entity_ids:
+            self.async_on_remove(
+                async_track_state_change_event(
+                    self.hass,
+                    self._opening_entity_ids,
+                    self._on_opening_entity_changed,
+                )
+            )
+
+        # Load the previous state if it's present
+        previous_state: State | None = await self.async_get_last_state()
+        if previous_state is not None:
+            _LOGGER.debug("Previous state found, loading data")
+            # Set the previous preset
+            if (
+                previous_preset := previous_state.attributes.get(ATTR_PRESET_MODE)
+            ) is not None and previous_preset in self._presets:
+                _LOGGER.debug("Previous state had preset %s", previous_preset)
+                self._current_preset = previous_preset
+                self._current_settings = self._presets[previous_preset].clone()
+            elif (
+                previous_settings := self._read_manual_settings(previous_state)
+            ) is not None:
+                _LOGGER.debug(
+                    "Previous state had manual settings %s", previous_settings
+                )
+                self._current_preset = None
+                self._current_settings = previous_settings
+            # Otherwise something is weird or we have no state so use the default which is set already
+
         # Startup function to run at HA startup or on creation, loads current values and old state
         @callback
         def _async_startup(*_) -> None:
@@ -281,6 +358,7 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
                 float(sensor_state.state) if sensor_state is not None else None
             )
 
+            # Set the current cooler siwtch state
             if self._cooler_switch_id is not None:
                 cooler_state: State | None = self.hass.states.get(
                     self._cooler_switch_id
@@ -291,6 +369,7 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
                     else False
                 )
 
+            # Set the current heater siwtch state
             if self._heater_switch_id is not None:
                 heater_state: State | None = self.hass.states.get(
                     self._heater_switch_id
@@ -301,16 +380,30 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
                     else False
                 )
 
+            # Set the current fan state
             if self._fan_switch_id is not None:
                 fan_state: State | None = self.hass.states.get(self._fan_switch_id)
                 self._is_fan_active = (
                     fan_state.state == STATE_ON if fan_state is not None else False
                 )
 
+            # Build the dictionary of opening states
+            if self._opening_entity_ids:
+                _LOGGER.debug("Initializing openings %s", self._opening_entity_ids)
+                for entity_id in self._opening_entity_ids:
+                    opening_state: State | None = self.hass.states.get(entity_id)
+                    self._current_opening_states[entity_id] = (
+                        opening_state in [STATE_OPEN, STATE_ON]
+                        if opening_state is not None
+                        else False
+                    )
+
+            self._is_initialized = True
+
             # Call update to get things going
             asyncio.run_coroutine_threadsafe(self.async_update(), self.hass.loop)
 
-        # Call the startup function either immediately if HA is running or wait until it is to run it
+        # Call the startup function immediately if HA is running or wait until it is to run it
         if self.hass.state == CoreState.running:
             _async_startup()
         else:
@@ -320,7 +413,6 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
     def extra_state_attributes(self):
         """Return entity specific state attributes to be saved."""
         data = {
-            ATTR_LAST_CYCLE: self._last_cycle,
             ATTR_MANUAL_HVAC_MODE: None,
             ATTR_MANUAL_FAN_MODE: None,
             ATTR_MANUAL_TEMP_LOW: None,
@@ -466,6 +558,7 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
         return (
             self._current_temp - self._current_settings.temp_high > self._temp_tolerance
             and self._current_settings.hvac_mode in self._valid_cool_hvac_modes
+            and self._is_any_opening_open is False
         )
 
     @property
@@ -473,6 +566,7 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
         return (
             self._current_settings.temp_low - self._current_temp > self._temp_tolerance
             and self._current_settings.hvac_mode in self._valid_heat_hvac_modes
+            and self._is_any_opening_open is False
         )
 
     @property
@@ -486,18 +580,38 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
             )
         )
 
+    @property
+    def _is_openings_value_locked(self) -> bool:
+        _LOGGER.debug("Opening lock expiry %s", self._openings_lock_expiry)
+        return (
+            self._openings_lock_expiry is not None
+            and self._openings_lock_expiry >= datetime.now(timezone.utc)
+        )
+
+    @property
+    def _is_any_opening_open(self) -> bool:
+        if self._is_openings_value_locked:
+            _LOGGER.debug("Openings value locked, using locked value")
+            return self._openings_locked_value
+        else:
+            return (
+                any(self._current_opening_states.values())
+                if self._current_opening_states is not None
+                else False
+            )
+
     async def async_update(self) -> None:
         """Performs the entity update procedure"""
-        delta: timedelta | None = (
-            datetime.now(timezone.utc) - self._last_cycle
-            if self._last_cycle is not None
-            else None
-        )
+        current_time = datetime.now(timezone.utc)
         cooler_changed: bool = False
         heater_changed: bool = False
         fan_changed: bool = False
 
-        if delta is None or delta > self._min_cycle_duration:
+        if self._is_initialized is False:
+            _LOGGER.debug("Not ready")
+            return
+
+        if self._cycle_lock_expiry is None or current_time >= self._cycle_lock_expiry:
             if self._is_cooling_needed is True:
                 cooler_changed |= await self._async_cooler_on()
                 if cooler_changed:
@@ -526,7 +640,7 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
                 _LOGGER.debug("Fan not required and was disabled")
 
         if cooler_changed or heater_changed:
-            self._last_cycle = datetime.now(timezone.utc)
+            self._cycle_lock_expiry = datetime.now(timezone.utc) + self._cycle_delay
 
         if cooler_changed or heater_changed or fan_changed:
             self.async_write_ha_state()
@@ -636,6 +750,45 @@ class YetAnotherSmartThermostat(ClimateEntity, RestoreEntity):
                 "Cooler switch changed and differs from current value, updating"
             )
             self._is_cooler_active = is_active
+
+    def _on_opening_entity_changed(self, event: Event) -> None:
+        new_state = event.data.get("new_state")
+        entity_id = event.data.get("entity_id")
+        is_open = (
+            new_state.state in [STATE_OPEN, STATE_ON]
+            if new_state is not None
+            else False
+        )
+
+        if self._current_opening_states.get(entity_id, False) != is_open:
+            # If there's no delay on the openings or it's expired, create a new one
+            if self._is_openings_value_locked is False:
+                self._openings_locked_value = self._is_any_opening_open
+                self._openings_lock_expiry = (
+                    datetime.now(timezone.utc) + self._opening_delay
+                )
+
+            _LOGGER.debug("Opening %s changed to state %s", entity_id, is_open)
+
+            self._current_opening_states[entity_id] = is_open
+            self.async_write_ha_state()
+
+    def _read_manual_settings(self, state: State) -> ClimateSettings:
+        """Reads the manually set values from the state into a ClimateSettings object"""
+        temp_low: float | None = state.attributes.get(ATTR_MANUAL_TEMP_LOW)
+        temp_high: float | None = state.attributes.get(ATTR_MANUAL_TEMP_HIGH)
+        hvac_mode: HVACMode = state.attributes.get(
+            ATTR_MANUAL_HVAC_MODE, self._default_hvac_mode
+        )
+        fan_mode: FanMode = state.attributes.get(
+            ATTR_MANUAL_FAN_MODE, self._default_fan_mode
+        )
+
+        return (
+            ClimateSettings(temp_low, temp_high, hvac_mode, fan_mode)
+            if temp_low is not None and temp_high is not None
+            else None
+        )
 
 
 class ClimateSettings:
